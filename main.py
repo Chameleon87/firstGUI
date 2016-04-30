d56de041c1f940088a7e95ef72bde914fd89e0c1
Chameleon87 <jessehodge1987@gmail.com>
2016-04-29 19:02:42 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+from Tkinter import *
+
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master
+        master.title("Address+")
+
+        self.label = Label(master, text="Welcome to Address+!")
+        self.label.pack()
+
+        self.new_contact_button = Button(master, text="Add Contact", command=self.new_contact)
+        self.new_contact_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def new_contact(self):
+        self.first_name_label = Label(master=None, text="First Name:")
+        self.first_name_label.pack(side=LEFT)
+
+        self.first_name_entry = Entry(master=None)
+        self.first_name_entry.pack(side=LEFT)
+
+        self.last_name_label = Label(master=None, text="Last Name:")
+        self.last_name_label.pack(side=LEFT)
+
+        self.last_name_entry = Entry(master=None)
+        self.last_name_entry.pack(side=LEFT)
+
+root = Tk()
+my_gui = MyFirstGUI(root)
+root.mainloop()
+
